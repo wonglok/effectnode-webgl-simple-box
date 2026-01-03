cf5f07cb82b8992afb410128075c5baeaa6ad990
Lok Lok <yellowhappy831@gmail.com>
2026-01-03 13:39:13 +0800

    save

--- src/effectnode/projects/living-room/codes/mymesh.jsx
+++ src/effectnode/projects/living-room/codes/mymesh.jsx
@@ -1,7 +1,6 @@
 import { Center, Environment, Html, MeshTransmissionMaterial, PerspectiveCamera, RoundedBox, Sphere } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { Suspense, useEffect, useState } from "react";
-import { EnergyArtCompo } from "./shared/EnergyArt/EnergyArtCompo";
 import { EnergyWaveCompo } from "./shared/EnergyWave/EnergyWaveCompo";
 
 export function ToolBox({ useNodeMemory }) {
@@ -57,7 +56,7 @@ function MyMesh({ useNodeMemory }) {
       <EnergyWaveCompo></EnergyWaveCompo>
     </group>
     <RoundedBox args={[2, 2, 2]} bevelSegments={5} radius={0.25} position={[0, 0, 0]}>
-      <MeshTransmissionMaterial transmission={1} roughness={0} thickness={1.75} color={baseColor} metalness={0} ></MeshTransmissionMaterial>
+      <MeshTransmissionMaterial anisotropicBlur={0.12} anisotropy={0.5} chromaticAberration={0.15} transmission={1} roughness={0} thickness={1.75} color={baseColor} metalness={0} ></MeshTransmissionMaterial>
     </RoundedBox>
   </>
 }
