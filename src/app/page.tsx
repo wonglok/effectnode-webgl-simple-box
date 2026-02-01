cc85307d5ecd81e6fe1ca17f5fd27807c39ad352
Lok Lok <yellowhappy831@gmail.com>
2026-02-01 10:33:08 +0800

    save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,22 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="w-full h-full relative">
-      <div className=" ">
+      <div className=" p-3 ">
         {/*  */}
         <ol>
-          <li>
+          <li className="p-3 underline">
             <Link href={`/edit`} prefetch>
-              Editor
+              3D Editor
             </Link>
           </li>
-          <li>
+          <li className="p-3 underline">
             <Link href={`/3d`} prefetch>
-              3D Page
+              3D Viewer
             </Link>
           </li>
         </ol>
+        {/*  */}
+        {/*  */}
         {/*  */}
       </div>
     </div>
