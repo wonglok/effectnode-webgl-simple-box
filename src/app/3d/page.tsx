739e05b894731bd848edf9db6b512da1832abbb2
Lok Lok <yellowhappy831@gmail.com>
2026-02-01 10:28:33 +0800

    save

---
+++ src/app/3d/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { CameraControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { EffectNode } from "effectnode-developer-tools/effectnode-runtime/EffectNode";
+import { Suspense } from "react";
+
+export default function Home() {
+  return (
+    <div className="w-full h-full relative">
+      <Canvas>
+        <Suspense fallback={null}>
+          <EffectNode projectName="living-room"></EffectNode>
+        </Suspense>
+
+        <CameraControls></CameraControls>
+      </Canvas>
+
+      <div className=" absolute bottom-0 right-0 px-3 py-3">
+        <div className="bg-white rounded-lg p-1 px-3">
+          <a
+            className="underline"
+            target="_blank"
+            href={`https://www.fab.com/listings/a926d36d-6460-4c6e-8aaa-c22667cb075f`}
+          >
+            3D Scene by Alister
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,25 @@
 "use client";
 
-import { CameraControls } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import { EffectNode } from "effectnode-developer-tools/effectnode-runtime/EffectNode";
-import { Suspense } from "react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="w-full h-full relative">
-      <Canvas>
-        <Suspense fallback={null}>
-          <EffectNode projectName="living-room"></EffectNode>
-        </Suspense>
-
-        <CameraControls></CameraControls>
-      </Canvas>
-
-      <div className=" absolute bottom-0 right-0 px-3 py-3">
-        <div className="bg-white rounded-lg p-1 px-3">
-          <a
-            className="underline"
-            target="_blank"
-            href={`https://www.fab.com/listings/a926d36d-6460-4c6e-8aaa-c22667cb075f`}
-          >
-            3D Scene by Alister
-          </a>
-        </div>
+      <div className=" ">
+        {/*  */}
+        <ol>
+          <li>
+            <Link href={`/edit`} prefetch>
+              Editor
+            </Link>
+          </li>
+          <li>
+            <Link href={`/3d`} prefetch>
+              3D Page
+            </Link>
+          </li>
+        </ol>
+        {/*  */}
       </div>
     </div>
   );
